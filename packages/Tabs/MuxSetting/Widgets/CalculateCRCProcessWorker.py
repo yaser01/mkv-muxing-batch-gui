19e1ef0e3f2d52d10f7f9653595efbc55c53ed77
Yasser Milato <yasser.milato@gmail.com>
2022-03-22 23:53:32 +0200

    Fix bug in calculate crc for non-mkv input files

--- packages/Tabs/MuxSetting/Widgets/CalculateCRCProcessWorker.py
+++ packages/Tabs/MuxSetting/Widgets/CalculateCRCProcessWorker.py
@@ -9,6 +9,12 @@ from packages.Startup import GlobalFiles
 from packages.Tabs.GlobalSetting import write_to_log_file
 
 
+def get_file_name_with_mkv_extension(file_name):
+    file_extension_start_index = file_name.rfind(".")
+    new_file_name_with_mkv_extension = file_name[:file_extension_start_index] + ".mkv"
+    return new_file_name_with_mkv_extension
+
+
 class CalculateCRCProcessWorker(QObject):
     crc_progress_signal = Signal(int)
     crc_result_signal = Signal(str)
@@ -26,8 +32,9 @@ class CalculateCRCProcessWorker(QObject):
         try:
             while not self.stop:
                 if not self.wait:
-                    file_size = getsize(self.file_name)
-                    with open(self.file_name, "rb") as f:
+                    file_name = get_file_name_with_mkv_extension(self.file_name)
+                    file_size = getsize(file_name)
+                    with open(file_name, "rb") as f:
                         checksum = 0
                         current_read = 0
                         current_percent = 0
