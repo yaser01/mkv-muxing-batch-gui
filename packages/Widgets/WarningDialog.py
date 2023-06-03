fbf8bf59c69cc1cff68dd607d7e7905db03f7240
yaser01 <ytarek463@gmail.com>
2023-06-03 12:02:55 +0300

    change position of ok button to center [previously  to right]

--- packages/Widgets/WarningDialog.py
+++ packages/Widgets/WarningDialog.py
@@ -17,13 +17,15 @@ class WarningDialog(QDialog):
         self.yesButton = QPushButton("OK")
 
         self.buttons_layout = QHBoxLayout()
+        self.buttons_layout.addWidget(QLabel(""))
         self.buttons_layout.addWidget(self.yesButton)
+        self.buttons_layout.addWidget(QLabel(""))
 
         self.main_layout = QGridLayout()
         self.main_layout.addWidget(self.messageIcon, 0, 0, 2, 1)
         self.main_layout.addWidget(QLabel(), 0, 1, 1, 1)  # add space
         self.main_layout.addWidget(self.message, 0, 2, 2, 3)
-        self.main_layout.addLayout(self.buttons_layout, 2, 4, 1, -1)
+        self.main_layout.addLayout(self.buttons_layout, 2, 2, 1, -1)
         self.main_layout.setContentsMargins(20, 20, 20, 20)
         self.setLayout(self.main_layout)
 
